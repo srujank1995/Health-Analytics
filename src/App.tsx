482f7a08bcb3b35b49631f9ab8b537bc36d68d89
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-05-10 01:06:34 +0530

    Table created in Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import './App.css';
+import TableData from './Table-Structure/Table-data';
 
 function App() {
   return (
     <div className="App">
-      
+      <TableData/>
     </div>
   );
 }
--- src/Table-Structure/Table-data.tsx
+++ src/Table-Structure/Table-data.tsx
@@ -1,8 +1,30 @@
+import Table from "react-bootstrap/Table";
 const TableData = () => {
 
     return(
         <div>
-
+        <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>SR No.</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+          <th>Blood Pressure</th>
+          <th>Sugar Level</th>
+          <th>Heart Rate</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        
+      </tbody>
+    </Table>
         </div>
     )
 }
