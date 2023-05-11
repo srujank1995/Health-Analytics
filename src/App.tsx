3435d8a8257efaee4c5e115cff503e1e9c7fca7b
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-05-11 17:04:26 +0530

    add context wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import { ContextHealthMainWrapper } from './Components/Context-Structure/ContextHealth-Main';
 import TableData from './Components/Table-Structure/Table-data';
 
 function App() {
   return (
+    <ContextHealthMainWrapper>
     <div className="App">
       <TableData/>
     </div>
+    </ContextHealthMainWrapper>
   );
 }
 
--- src/Components/Context-Structure/ContextHealth-Main.tsx
+++ src/Components/Context-Structure/ContextHealth-Main.tsx
@@ -1,17 +1,9 @@
 import { Children, createContext, useState } from "react";
 
-const ContextHealthMainWrapper:React.FC<any>=(props)=>{
-   
-    const {children} = props;
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
 const ContextHealthMain= createContext({
-    Users:[{
-        SR_NO:0,
+     users:[{
+        id:0,
         name:'',
         BP:0,
         HR:0,
@@ -22,4 +14,25 @@ const ContextHealthMain= createContext({
 
     }]
 })
+
+export const ContextHealthMainWrapper:React.FC<any>=(props)=>{
+   const users=[{id:0,
+    name:'',
+    BP:0,
+    HR:0,
+    SBP:0,
+    DBP:0,
+    SR:0,
+    SL:0,}]
+    const {children} = props;
+    return (
+        <ContextHealthMain.Provider value={
+            {users:users}
+        }>
+        <div>
+            {children}     
+        </div>
+        </ContextHealthMain.Provider>
+    )
+}
 export default ContextHealthMain;
