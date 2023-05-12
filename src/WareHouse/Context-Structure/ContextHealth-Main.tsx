e3ae9b5947fd122a8a8bb1d5fb0f92fcfe52a29f
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-05-12 23:39:15 +0530

    IntialState added to context

--- src/WareHouse/Context-Structure/ContextHealth-Main.tsx
+++ src/WareHouse/Context-Structure/ContextHealth-Main.tsx
@@ -18,13 +18,15 @@ const ContextHealthMain= createContext({
 export const ContextHealthMainWrapper:React.FC<any>=(props)=>{
    const users: StateType[]=[{
     id:0,
-    name:'',
-    BP:0,
-    HR:0,
-    SBP:0,
-    DBP:0,
-    SR:0,
-    SL:0,}]
+    name:"",
+    BP: { value: 0, name: "Blood Pressure" },
+    HR: { value: 0, name: "Heart Rate" },
+    SBP: { value: 0, name: "Sistolic Blood Pressure" },
+    DBP: { value: 0, name: "Distolic Blood Pressure" },
+    BTemp: { value: 0, name: "Body Temperature" },
+    SR: { value: 0, name: "Sugar level" },
+    SL: { value: 0, name: "Stress level" },
+    }]
     const {children} = props;
     return (
         <ContextHealthMain.Provider value={
