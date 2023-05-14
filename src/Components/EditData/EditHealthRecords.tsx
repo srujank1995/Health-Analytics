59d9f5bc2e2f78ed6b2db2cde4277d5759d8da20
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-05-14 12:53:17 +0530

    Edit function created

--- src/Components/EditData/EditHealthRecords.tsx
+++ src/Components/EditData/EditHealthRecords.tsx
@@ -1,18 +1,73 @@
-import { useContext } from 'react'
-import { StateType, Update_BP } from '../../Context-Structure/StateTypeMain'
-import ContextHealthMain from '../../Context-Structure/ContextHealth-Main'
+import { useContext, useRef } from "react";
+import {
+  StateType,
+  Update_BP,
+  Update_BTemp,
+  Update_DBP,
+  Update_HR,
+  Update_SBP,
+  Update_SL,
+  Update_SR,
+} from "../../Context-Structure/StateTypeMain";
+import ContextHealthMain from "../../Context-Structure/ContextHealth-Main";
 
 const EditHealthRecords = () => {
-  const {editUserData} = useContext(ContextHealthMain)
+  const ModalRef: any = useRef();
+  const { editUserData, ModalFun, setInputData, initialValue } =useContext(ContextHealthMain);
   return (
     <div>
-
-      <input type='text' value={editUserData.id} />
-      <select id='health'>
+      <ul>
+        <li>
+          {editUserData.BP.name} : {editUserData.BP.value}
+        </li>
+        <li>
+          {editUserData.HR.name} : {editUserData.HR.value}
+        </li>
+        <li>
+          {editUserData.SBP.name} : {editUserData.SBP.value}
+        </li>
+        <li>
+          {editUserData.DBP.name} : {editUserData.DBP.value}
+        </li>
+        <li>
+          {editUserData.BTemp.name} : {editUserData.BTemp.value}
+        </li>
+        <li>
+          {editUserData.SR.name} : {editUserData.SR.value}
+        </li>
+        <li>
+          {editUserData.SL.name} : {editUserData.SL.value}
+        </li>
+      </ul>
+      <label>ID:-</label>
+      <br></br>
+      <input type="number" value={editUserData.id} />
+      <br></br>
+      <input type="text" value={editUserData.name} />
+      <br></br>
+      <input
+        type="number"
+        value={initialValue}
+        onChange={(e) => {
+          setInputData(parseInt(e.target.value));
+        }}
+      />
+      <br></br>
+      <select id="health" ref={ModalRef}>
         <option value={Update_BP}>{editUserData.BP.name}</option>
+        <option value={Update_HR}>{editUserData.HR.name}</option>
+        <option value={Update_SBP}>{editUserData.SBP.name}</option>
+        <option value={Update_DBP}>{editUserData.DBP.name}</option>
+        <option value={Update_BTemp}>{editUserData.BTemp.name}</option>
+        <option value={Update_SR}>{editUserData.SR.name}</option>
+        <option value={Update_SL}>{editUserData.SL.name}</option>
       </select>
+      <br></br>
+      <button type="button" onClick={() => ModalFun(ModalRef.current.value)}>
+        Submit Details
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default EditHealthRecords
+export default EditHealthRecords;
--- src/Context-Structure/ContextHealth-Main.tsx
+++ src/Context-Structure/ContextHealth-Main.tsx
@@ -1,13 +1,17 @@
 import { Children, createContext, useReducer, useState } from "react";
 import ReducerFunction, { initialstate } from "./ReducerFunction";
 import { StateType, VIEW_EDIT, stateval } from "./StateTypeMain";
+import { Value } from "sass";
 
 const ContextHealthMain = createContext({
+  initialValue:0,
   UserVal: initialstate,
   currentStatePage: "View",
   CurrentpageFn: (value: VIEW_EDIT) => {},
   editUserData:initialstate[0],
-  EditDataFn:(userData:StateType) => {}
+  EditDataFn:(userData:StateType) => {},
+  ModalFun:(value:any) => {},
+  setInputData:(value:number) => {}
 
 });
 
@@ -24,15 +28,29 @@ export const ContextHealthMainWrapper: React.FC<any> = (props) => {
   const EditDataFn = (userData:StateType) => {
     setEditUserData(userData)
   }
+  const [inputData, setInputData] = useState<number>(0)
+  
+  const ModalFun = (value:any) => {
+   dispatch({
+    payload:{value:inputData},
+    type:value
+   }) 
+  }
+  const SetInputDataFn = (Value:number) =>{
+    setInputData(Value)
+  }
   const { children } = props;
   return (
     <ContextHealthMain.Provider
       value={{
+        ModalFun,
+        setInputData:SetInputDataFn,
+        initialValue:inputData,
         UserVal: initialstate,
         currentStatePage: currentStatePage,
         CurrentpageFn: CurrentpageFn,
         editUserData:editUserData,
-        EditDataFn:EditDataFn
+        EditDataFn:EditDataFn,
       }}
     >
       <div>{children}</div>
